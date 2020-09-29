26afe914764df9cf6a57f9aa09ce7168bb22081d
Artem Farafonov <farafonoff@gmail.com>
2020-09-29 04:16:05 +0400

    prod build for gh pages

---
+++ build/precache-manifest.c6944558c778dca147d5b59683decf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52e6a8ce6923a8dac9fed24af431e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeaceb2c55f1c374e720",
+    "url": "/static/css/main.afc6cd7a.chunk.css"
+  },
+  {
+    "revision": "0ee1bf43d0f909fbf526",
+    "url": "/static/js/2.40110c86.chunk.js"
+  },
+  {
+    "revision": "32d9c3460ee62d17fcc60d35208e458b",
+    "url": "/static/js/2.40110c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeaceb2c55f1c374e720",
+    "url": "/static/js/main.3f3ba21b.chunk.js"
+  },
+  {
+    "revision": "9e9f59bc18d85284cfc6",
+    "url": "/static/js/runtime-main.0fda9b16.js"
+  }
+]);
