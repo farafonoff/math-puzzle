418d458981530f75730250630735b7fceae8b8fc
Artem Farafonov <farafonoff@gmail.com>
2020-09-29 04:31:50 +0400

    Updates

---
+++ precache-manifest.668b05d3cbf060b3cced59f1cbc0bbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296282ff42e8f8f16ec210833c630f8d",
+    "url": "/math-puzzle/index.html"
+  },
+  {
+    "revision": "43248b3a80856844e475",
+    "url": "/math-puzzle/static/css/main.afc6cd7a.chunk.css"
+  },
+  {
+    "revision": "0ee1bf43d0f909fbf526",
+    "url": "/math-puzzle/static/js/2.40110c86.chunk.js"
+  },
+  {
+    "revision": "32d9c3460ee62d17fcc60d35208e458b",
+    "url": "/math-puzzle/static/js/2.40110c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43248b3a80856844e475",
+    "url": "/math-puzzle/static/js/main.44ddd21a.chunk.js"
+  },
+  {
+    "revision": "2501c1291a13e513dac7",
+    "url": "/math-puzzle/static/js/runtime-main.e72cd6e8.js"
+  }
+]);
